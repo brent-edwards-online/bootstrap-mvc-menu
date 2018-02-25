44b9b764b2d89429320b55b85c8f392bc92c84d5
brent-edwards-online <brent@brentedwardsonline.com>
2018-02-25 15:06:29 +1000

    Initial Commit

---
+++ BootstrapMVCMenu/Models/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapMVCMenu.Models
+{
+    public class MenuItem
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
